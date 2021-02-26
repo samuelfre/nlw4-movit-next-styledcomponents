b1cc9203ab68b33f0710516318684fe58be98605
samuelfre <samuel.hgs19@outlook.com>
2021-02-25 21:08:16 -0300

    .

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -1,4 +1,8 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+
+interface PropsType{
+  
+}
 
 const Div = styled.div`
   /* ... */
@@ -12,48 +16,128 @@ const Div = styled.div`
   align-items: center;
   justify-content: center;
   text-align: center;
+`;
+
+const Button = styled.button`
+  height: 3rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: 0;
+  border-radius: 5px;
+  color: var(--white);
+  font-size: 1rem;
+  font-weight: 600;
+  transition: 0.2s;
+  :hover{
+    filter: brightness(0.9);
+  }
+  ${props => props.fail ? css`background: var(--red);` : css`background: var(--green);`}
+`;
+
+
+const InnerDiv = styled.div`
+  /* ... */
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+
+  header {
+    color: var(--blue);
+    font-weight: 600;
+    font-size: 1.25rem;
+    padding: 0 2rem 1.5rem;
+    border-bottom: 1px solid var(--gray-line);
+  }
+
+  main {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+    strong {
+      font-weight: 600;
+      font-size: 2rem;
+      color: var(--title);
+      margin: 1.5rem 0 1rem;
+    }
+
+    p {
+      line-height: 1.5;
+    }
+  }
+
+  footer {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: 1rem;
+
+    
+
+  }
 
 `;
+
 const DivNotActive = styled.div`
   /* ... */
   display: flex;
   flex-direction: column;
   align-items: center;
 
-`;
-const Strong = styled.strong`
-  /* ... */
-  font-size: 1.5rem;
-  font-weight: 500;
-  line-height: 1.4;
-`;
+  strong {
+    font-size: 1.5rem;
+    font-weight: 500;
+    line-height: 1.4;
+  }
 
-const P = styled.p`
-  /* ... */
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  line-height: 1.4;
-  max-width: 70%;
-  margin-top: 3rem;
-`;
-
-const IMG = styled.img`
-  /* ... */
-  margin-bottom: 1rem;
+  p {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    line-height: 1.4;
+    max-width: 70%;
+    margin-top: 3rem;
+  }
+  
+  img {
+    margin-bottom: 1rem;
+  }
 `;
 
 
 export function ChallengeBox() {
+  const hasActiveChallenge = true;
   return (
     <Div>
-      <DivNotActive>
-        <Strong>Finalize um ciclo para receber um desafio</Strong>
-        <P>
-          <IMG src="icons/level-up.svg" alt="Level up" />
+      {hasActiveChallenge ? (
+        <InnerDiv>
+          <header>Ganhe 400 xp</header>
+          <main>
+            <img src="icons/body.svg" />
+            <strong>Novo desafio</strong>
+            <p>Levante e faça uma caminhada de 3 minutos</p>
+          </main>
+          <footer>
+            <Button
+              type="button"
+              fail
+            >Falhei</Button>
+            <Button
+              type="button"
+            >Completei</Button>
+          </footer>
+        </InnerDiv>
+      ) : (
+          <DivNotActive>
+            <strong>Finalize um ciclo para receber um desafio</strong>
+            <p>
+              <img src="icons/level-up.svg" alt="Level up" />
           Avance de level completando desafios.
-        </P>
-      </DivNotActive>
+        </p>
+          </DivNotActive>
+        )}
     </Div>
   );
 }
