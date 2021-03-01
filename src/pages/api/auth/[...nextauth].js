0dbe9a3a8e25447aab81d6c77d73892d9f17b6fa
samuelfre <samuel.hgs19@outlook.com>
2021-03-01 17:04:21 -0300

    .

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,8 +1,7 @@
 import NextAuth from 'next-auth'
 import Providers from 'next-auth/providers'
-import { NextApiRequest, NextApiResponse } from 'next'
 
-export default (req: NextApiRequest, res: NextApiResponse) => NextAuth(req, res, {
+export default NextAuth({
   // Configure one or more authentication providers
   providers: [
     Providers.GitHub({
