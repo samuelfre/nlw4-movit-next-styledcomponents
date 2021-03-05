12b13bc19fe3ea6b380da86e9eb69ab99db7dcc6
samuelfre <samuel.hgs19@outlook.com>
2021-03-05 20:15:43 -0300

    .

--- src/components/LateralBar.tsx
+++ src/components/LateralBar.tsx
@@ -1,47 +1,107 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components';
 
 const Div = styled.div`
   /* ... */
   background: linear-gradient(180deg, #FFFFFF 0%, rgba(255, 255, 255, 0) 100%);
-  filter: drop-shadow(0px 0px 60px rgba(0, 0, 0, 0.05));
   height: 100vh;
   width: 7rem;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
+
+  .div{
+    margin-top: 2rem;
+    display: flex;
+    justify-content: center;
+  }
 
   .logo-crop{
     width: 3rem;
     height: 2.6rem;
   }
   .home{
-    width: 4.5rem;
-    height: 3.5rem;
+    width: 2rem;
+    height: 2rem;
   }
   .ranking{
-    width: 4.5rem;
-    height: 3.5rem;
-    background-color: red;
+    width: 2rem;
+    height: 2rem;
   }
+  .button{
+    border: none;
+    outline: none;    
+    cursor: pointer;
+    display: flex;    
+    width: 100%;
+    background: transparent;
+    align-items: center;
+    justify-content: center;
+  }
+  .button2{
+    border: none;
+    outline: none;    
+    cursor: pointer;
+    background: transparent;
+  }
+  .hover{
+    position: absolute;
+    left: 0;
+    background: ${props => props.isSelected ? `var(--blue)` : 'transparent'};
+    width: 0.25rem;
+    height: 3.5rem;
+  }  
+  .hover1{
+    position: absolute;
+    left: 0;
+    background: ${props => !props.isSelected ? `var(--blue)` : 'transparent'};
+    width: 0.25rem;
+    height: 3.5rem;
+  }  
+  
+  .button:hover > .hover { 
+    
+      background: var(--blue); }
+
+  .div1{
+    margin-top: 17.3rem;    
+    display: flex;
+    flex-direction: column;
+  }
+  
 
 `;
 
 
 export default function LateralBar() {
+  const [isSelected, setisSelected] = useState(true);
   return (
-    <Div>
-      <div>
-        <img className='logo-crop' src="logo-crop.svg"/>
+    <Div isSelected = {isSelected}>
+      <div className="div">
+        <button className="button2" >
+          <img
+            className='logo-crop'
+            src="logo-crop.svg" />
+        </button>
       </div>
-      <div>
-        <img className='home' src="home.svg"/>
+
+      <div className="div1">
+        <button className="button" onClick={() => setisSelected(true)}>
+          <div className='hover' />
+          <div>
+            <img className='home' src={isSelected ? 'home-blue.svg' : 'home.svg'} />
+          </div>
+        </button>
+
+
+        <div style={{ marginTop: '1.75rem' }}>
+          <button className="button"  onClick={() => setisSelected(false)}>
+            <div className='hover1' />
+            <div>
+              <img className='ranking' src={!isSelected ? 'ranking-blue.svg' : 'ranking.svg'} />
+            </div>
+          </button>
+
+        </div>
       </div>
-      <div>
-        <img className='ranking' src="ranking.svg"/>
-      </div>
-      
+
     </Div>
   )
 }
